6cfc32efbc472face17dfebce966aea1cbd34fcc
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-06-13 08:07:13 +0530

    Added Components, Routes, Footer

---
+++ food-delivery-system/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+
+const Footer = ()=>{
+    return(
+        <>
+            <div >
+                <div >
+                    © 2020 Grab Food. All Rights Reserved.
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Footer; 
